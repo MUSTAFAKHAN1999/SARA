d541299ae407bd096d62379f3cb0aca852772723
MUSTAFAKHAN1999 <113144060+MUSTAFAKHAN1999@users.noreply.github.com>
2023-12-18 16:29:19 -0600

    Update Config.py

--- Config.py
+++ Config.py
@@ -2,7 +2,7 @@ import chromadb
 
 client = client = chromadb.PersistentClient(path="./database")
 
-OPENAI_API_KEY = "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"
+OPENAI_API_KEY = "<YOUR OPENAI API KEY>"
 
 EMBEDDING_MODEL = "text-embedding-ada-002"
 
